f03b71177d6b9ec20804ca8d03dfbbf8b80950f0
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-29 09:30:55 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 15.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int? {
+    guard num1 > 0, num1 <= 100, num2 > 0, num2 <= 100 else {
+        print("0 < num1 ≤ 100 그리고 0 < num2 ≤ 100이어야 합니다.")
+        return nil
+    }
+    return num1 / num2
+}
