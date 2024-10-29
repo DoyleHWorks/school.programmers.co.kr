c9e0baef6beb933a9dbd20a61be4e6068a3dd073
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-29 09:24:40 +0900

    [level 0] Title: 두 수의 곱, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.swift
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 func solution(_ num1:Int, _ num2:Int) -> Int? {
-    guard num1 >= 0, num1 <= 100, num2 >= 0, num2 <= 100 else {
+    guard (0...100).contains(num1), (0...100).contains(num2) else {
         print("0 ≤ num1 ≤ 100 그리고 0 ≤ num2 ≤ 100이어야 합니다.")
         return nil
     }
