ed2f38fc18068bbc5479ccd65298333f5151f8ba
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-30 09:53:33 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.11 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ n:Int) -> Int? {
+    guard (1...100000000).contains(n) else {
+        print("(입력값)의 범위 : 100,000,000 이하의 자연수이어야 합니다.")
+        return nil
+    }
+    let digits = String(n).map { Int(String($0))! }
+    return digits.reduce(0, +)
+}
