b63220aa67e350042ead1373017db1e723cfbeb8
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-30 10:04:31 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/README.md
+++ 프로그래머스/1/12931. 자릿수 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자릿수 더하기 - 12931 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12931?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 0.11 ms
+메모리: 16.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 09:53:30
+2024년 10월 30일 10:04:28
 
 ### 문제 설명
 
--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -5,6 +5,13 @@ func solution(_ n:Int) -> Int? {
         print("(입력값)의 범위 : 100,000,000 이하의 자연수이어야 합니다.")
         return nil
     }
-    let digits = String(n).map { Int(String($0))! }
-    return digits.reduce(0, +)
+    // let digits = String(n).map { Int(String($0))! }
+    // return digits.reduce(0, +)
+    var answer = 0
+    var num = n
+    while num > 0 {
+        answer += num % 10
+        num = num / 10
+    }
+    return answer
 }
