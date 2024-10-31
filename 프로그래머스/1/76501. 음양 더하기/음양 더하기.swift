a75f5d3b09b350f8d85824e02ff57336933b964a
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-31 20:19:33 +0900

    [level 1] Title: 음양 더하기, Time: 0.83 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -5,8 +5,17 @@ func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int? {
         print("절댓값 배열의 길이는 1 이상 1,000 이하이며, 부호 배열의 길이는 절댓값 배열의 길이와 같아야 합니다.")
         return nil
     }
-    
-    let numbers = absolutes.enumerated().map({signs[$0] ? $1 : 0 - $1}) 
-    let sumOfNumbers = numbers.reduce(0, +)
-    return sumOfNumbers 
+     
+    // 제일 느렸던 방법
+    // let sumOfNumbers = zip(absolutes, signs)
+    //     .map({$1 ? $0 : -$0})
+    //     .reduce(0, +)
+
+    // 두 번째로 빨랐던 방법
+    // let numbers = absolutes.enumerated().map({signs[$0] ? $1 : 0 - $1}) 
+    // let sumOfNumbers = numbers.reduce(0, +)
+
+    let sumOfNumbers = (0..<absolutes.count).map({ signs[$0] ? absolutes[$0] : -absolutes[$0] }).reduce(0, +)
+
+    return sumOfNumbers
 }
