3c317be51d871f1cf250a3e09391e473fc50193a
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-10-31 11:53:43 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.09 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.swift
@@ -1,37 +1,19 @@
 func solution(_ s:String) -> Int? {
     guard (1...5).contains(s.count) else {
-        print("(입력값)의 길이는 1 이상 5 이하이어야 합니다.")
+        print("입력값의 길이는 1 이상 5 이하이어야 합니다.")
         return nil
     }
     
-    var numberString = s
-    var isPositive: Bool
-    
-    switch numberString[numberString.startIndex] {
-    case "0":
-        print("입력값은 \"0\"으로 시작할 수 없습니다.")
-        return nil
-    case "+": 
-        isPositive = true
-        numberString.removeFirst()
-    case "-": 
-        isPositive = false
-        numberString.removeFirst()
-    default: 
-        isPositive = true
-    }
-    
-    var numberInt: Int?
-    
-    if var intValue = Int(numberString) {
-        if isPositive == false {
-            intValue.negate()
-        }
-        numberInt = intValue
-    } else {
-        print("유효하지 않은 입력값입니다.")
+    if s.hasPrefix("0") {
+        print("입력값은 '0'으로 시작할 수 없습니다.")
         return nil
     }
     
-    return numberInt
+    let validCharacters = Set("0123456789+-")
+    if !s.allSatisfy({ validCharacters.contains($0) }) {
+        print("입력값은 부호(+, -)와 숫자로만 이루어져야 합니다.")
+        return nil
+    }
+    
+    return Int(s)
 }
