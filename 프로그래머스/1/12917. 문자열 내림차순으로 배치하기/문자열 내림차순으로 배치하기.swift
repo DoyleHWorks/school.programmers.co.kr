476d88cee2cd13e374a64cedb2ecb6bab5f90335
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-01 13:15:33 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 3.20 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
@@ -0,0 +1,34 @@
+func solution(_ str: String) -> String {
+    // Algorithm 1
+    /*
+    let lowercases = Set("abcdefghijklmnopqrstuvwxyz")
+    let uppercases = Set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    let validCharacters = lowercases.union(uppercases)
+    
+    guard str.count >= 1 && str.allSatisfy({validCharacters.contains($0)}) else {
+        print("입력값은 길이 1 이상인 문자열이고, 영문 대소문자로만 구성되어야 합니다.")
+        return ""
+    }
+
+    let strLowercasesSorted = str.filter({lowercases.contains($0)}).sorted(by: >)
+    let strUppercasesSorted = str.filter({uppercases.contains($0)}).sorted(by: >)
+    let sortedString = String(strLowercasesSorted + strUppercasesSorted)
+    return sortedString
+    */
+    
+    //Algorithm 2
+    guard !str.isEmpty && str.allSatisfy({ $0.isLetter }) else {
+        print("입력값은 길이 1 이상인 문자열이고, 영문 대소문자로만 구성되어야 합니다.")
+        return ""
+    }
+    
+    let (lowercases, uppercases) = str.sorted(by: >).reduce(into: ([Character](), [Character]())) { result, char in 
+        if char.isLowercase {
+            result.0.append(char)
+        } else {
+            result.1.append(char)
+        }
+    }
+    var sortedString = String(lowercases) + String(uppercases)
+    return sortedString
+}
