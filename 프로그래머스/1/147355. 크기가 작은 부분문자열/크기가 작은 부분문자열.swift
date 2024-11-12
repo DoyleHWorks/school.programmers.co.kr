f4b821899d575da64e005799481c5fa16cef8281
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-12 19:23:58 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 622.43 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.swift
@@ -2,24 +2,41 @@ import Foundation
 
 func solution(_ t: String, _ p: String) -> Int {
 
-    var mutableT = t
-    var resultCount = 0
-    let intP = Int(p)!
+//     var mutableT = t
+//     var resultCount = 0
+//     let intP = Int(p)!
     
-    while mutableT.count != p.count {
-        var numString = ""
+//     while mutableT.count != p.count {
+//         var numString = ""
         
-        for char in mutableT {
-            numString.append(char)
-            if numString.count == p.count { break }
+//         for char in mutableT {
+//             numString.append(char)
+//             if numString.count == p.count { break }
+//         }
+        
+//         if Int(numString)! <= intP { resultCount += 1 }
+        
+//         mutableT.removeFirst()
+//     }
+    
+//     if Int(mutableT)! <= intP { resultCount += 1 }
+    
+//     return resultCount
+    
+    let intP = Int(p)!
+    let pCount = p.count
+    var resultCount = 0
+    
+    for i in 0...(t.count - pCount) {
+        let startIndex = t.index(t.startIndex, offsetBy: i)
+        let endIndex = t.index(startIndex, offsetBy: pCount)
+        let substring = String(t[startIndex..<endIndex])
+        
+        if let intSubstring = Int(substring), intSubstring <= intP {
+            resultCount += 1
         }
-        
-        if Int(numString)! <= intP { resultCount += 1 }
-        
-        mutableT.removeFirst()
     }
     
-    if Int(mutableT)! <= intP { resultCount += 1 }
-    
     return resultCount
+
 }
