c4151e65d132a79c3c85de99a7d5f83617f5eb5d
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-18 09:57:54 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 15.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ clothes: [[String]]) -> Int {
+    var clothesCategorized: [String:Int] = [:]
+    
+    for i in clothes.indices {
+        clothesCategorized[clothes[i][1], default: 0] += 1
+    }
+
+    let combinationCount = clothesCategorized
+        .map({ $0.value })
+        .map({ $0 + 1 })
+        .reduce(1, *)
+    
+    return combinationCount - 1
+}
