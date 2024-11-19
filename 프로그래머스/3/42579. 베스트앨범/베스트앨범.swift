ed112aa1c3d0a6d85e90cfa1fe30b243042d18c9
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-19 14:48:56 +0900

    [level 3] Title: 베스트앨범, Time: 1.65 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -4,23 +4,24 @@ func solution(_ genres: [String], _ plays: [Int]) -> [Int] {
 	// 곡 정보 (Key: 곡 고유번호, Value: (장르, 재생 수))
     var songInfo = Dictionary(uniqueKeysWithValues: zip(genres.indices, zip(genres, plays)))
     
-    // 장르별 재생수 취합 (Key: 장르, Value: 재생수)
+    // 장르별 재생수 취합
     var totalPlaysByGenre: [String: Int] = [:]
     for info in songInfo.values {
         let genre = info.0, plays = info.1
         totalPlaysByGenre[genre, default: 0] += plays
     }
-    
-    // 장르 순위
-    let genreRank = totalPlaysByGenre.keys.sorted {
+    let genreByPlays = totalPlaysByGenre.keys.sorted {
     	totalPlaysByGenre[$0]! > totalPlaysByGenre[$1]!
     }
+    
+    // 장르 순위 (Key: 장르, Value: 장르 순위)
+    let genreRank = Dictionary(uniqueKeysWithValues: zip(genreByPlays, genreByPlays.indices))
     
     // 곡 순위
     let songSorted = songInfo
         .sorted { $0.key < $1.key } // 고유 번호 정렬
         .sorted { $0.value.1 > $1.value.1 } // 재생 수 정렬
-        .sorted { genreRank.firstIndex(of: $0.value.0)! < genreRank.firstIndex(of: $1.value.0)! } // 장르 순위 정렬
+        .sorted { genreRank[$0.value.0, default: -1] < genreRank[$1.value.0, default: -1] } // 장르 순위 정렬
 
 	// 베스트앨범 추출
     var resultAlbum: [Int] = []
