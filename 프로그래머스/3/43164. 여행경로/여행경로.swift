90b6d896fed1d1ddbba499d5db44528520eea940
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-11-28 09:20:18 +0900

    [level 3] Title: 여행경로, Time: 0.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.swift
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-func solution(_ tickets:[[String]]) -> [String] {
+func solution(_ tickets: [[String]]) -> [String] {
     var routes: [String:[String]] = [:]
     let ticketCount = tickets.count
     
@@ -10,21 +10,21 @@ func solution(_ tickets:[[String]]) -> [String] {
     }
     
     for key in routes.keys {
-        routes[key]?.sort() // 조건 2: 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 합니다.
+        routes[key]?.sort() // 조건: 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return함
     }
     
     func dfs(_ current: String, _ path: inout [String]) -> Bool {
-        if path.count == ticketCount + 1 { // 조건 1: 주어진 항공권은 모두 사용해야 합니다.
+        if path.count == ticketCount + 1 { // 조건: 주어진 항공권은 모두 사용해야 함
             return true
         }
         
-        guard let destinations = routes[current], !destinations.isEmpty else { return false }
+        guard let destinations = routes[current] else { return false } // 다음 목적지가 없다면 돌아가기
         
         for (index, next) in destinations.enumerated() {
             routes[current]?.remove(at: index)
             path.append(next)
             
-            if dfs(next, &path) {
+            if dfs(next, &path) { // 항공권을 모두 썼는지 체크 & 다음 경로 확인
                 return true
             }
             
@@ -35,7 +35,7 @@ func solution(_ tickets:[[String]]) -> [String] {
         return false
     }
     
-    var path: [String] = ["ICN"]
+    var path: [String] = ["ICN"] // 조건: 항상 "ICN" 공항에서 출발
     _ = dfs("ICN", &path)
     return path
 }
