5c440fbc4233fb8d1889dc312495ceb012ded713
Maccha <129703581+DoyleHWorks@users.noreply.github.com>
2024-12-03 10:06:55 +0900

    [level 2] Title: 피로도, Time: 155.21 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -3,21 +3,22 @@ import Foundation
 func solution(_ k: Int, _ dungeons: [[Int]]) -> Int {
     
     var result: Int = 0
+    var kMutable = k
     var visited: [Bool] = Array(repeating: false, count: dungeons.count)
 
-    func explore(_ k: Int, _ visited: [Bool], _ count: Int) {
+    func explore(_ kMutable: Int, _ visited: [Bool], _ count: Int) {
         result = max(result, count)
         
         for (index, dungeon) in dungeons.enumerated() {
             if visited[index] { continue }
-            if k < dungeon[0] { continue }
+            if kMutable < dungeon[0] { continue }
             
             var newVisited = visited
             newVisited[index] = true
-            explore(k - dungeon[1], newVisited, count + 1)
+            explore(kMutable - dungeon[1], newVisited, count + 1)
         }
     }
     
-    explore(k, visited, 0)
+    explore(kMutable, visited, 0)
     return result
 }
